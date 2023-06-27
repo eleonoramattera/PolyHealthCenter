6e20391ed2812a15bd04c6c2d484bebf94d1cd9e
Eleonora Mattera <m.eleonora98@live.it>
2023-06-27 17:32:36 +0200

    utenti(model, runner, service, repo, controller)

---
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/PolyHealthCenterApplication.java
@@ -0,0 +1,13 @@
+package PolyHealthCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyHealthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyHealthCenterApplication.class, args);
+	}
+
+}
---
+++ PolyHealthCenter/src/test/java/PolyHealthCenter/PolyHealthCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package PolyHealthCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyHealthCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
