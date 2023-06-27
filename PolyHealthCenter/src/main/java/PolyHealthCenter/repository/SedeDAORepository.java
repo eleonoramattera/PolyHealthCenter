7157c5491e7908732cc6bb010136b3ed11a7dbee
Eleonora Mattera <m.eleonora98@live.it>
2023-06-27 20:16:33 +0200

    sede(model, service, repo, runner, controller)

---
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/repository/SedeDAORepository.java
@@ -0,0 +1,9 @@
+package PolyHealthCenter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import PolyHealthCenter.model.Sede;
+
+public interface SedeDAORepository extends JpaRepository<Sede, Long> {
+
+}
