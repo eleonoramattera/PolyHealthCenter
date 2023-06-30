9d30d2fc2bad1b09c401b64ef7e1ca991da8a4ef
Eleonora Mattera <m.eleonora98@live.it>
2023-06-30 20:24:14 +0200

    prenotazione model

--- PolyHealthCenter/src/main/java/PolyHealthCenter/model/Prenotazione.java
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/model/Prenotazione.java
@@ -1,12 +1,16 @@
 package PolyHealthCenter.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.ManyToMany;
@@ -28,8 +32,6 @@ public class Prenotazione {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@ManyToOne
-	private Utente utente;
 	
 	@OneToOne
 	private Terapia terapia;
@@ -39,7 +41,20 @@ public class Prenotazione {
 	
 	@Column(nullable=false,name="data_prenotazione")
 	private LocalDate dataPrenotazione;
-
+	
+	@ManyToOne
+	@JoinTable(name = "prenotazione_utente",
+     joinColumns = @JoinColumn(name = "id_prenotazione"),
+	  inverseJoinColumns = @JoinColumn(name = "id_utente"))
+	private Utente utente;
+	
+//	@JsonIgnore
+//	@ManyToMany
+//	@JoinTable(name = "prenotazione_utente",
+//	           joinColumns = @JoinColumn(name = "id_prenotazione"),
+//			  inverseJoinColumns = @JoinColumn(name = "id_utente"))
+//	private List<Utente> utente;
+//	
 	public Prenotazione(Utente utente, Terapia terapia, LocalDate dataPrenotazione) {
 		super();
 		this.utente = utente;
