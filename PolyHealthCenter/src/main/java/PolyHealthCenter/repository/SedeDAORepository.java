2ac711888f0554bf0618349ee1fe204fda62efde
Eleonora Mattera <m.eleonora98@live.it>
2023-07-03 22:16:58 +0200

    fix sede service, sede repo, sede controller

--- PolyHealthCenter/src/main/java/PolyHealthCenter/repository/SedeDAORepository.java
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/repository/SedeDAORepository.java
@@ -10,7 +10,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import PolyHealthCenter.model.Sede;
 
 public interface SedeDAORepository extends JpaRepository<Sede, Long>,  PagingAndSortingRepository<Sede, Long>{
-
+   
+	public boolean existsByComune(String comune);
+	
 	Page<Sede> findByComune(String comune, Pageable pageable);
 	Page<Sede> findByRegione(String regione, Pageable pageable);
 }
--- PolyHealthCenter/src/main/java/PolyHealthCenter/service/SedeService.java
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/service/SedeService.java
@@ -5,16 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import PolyHealthCenter.model.Feedback;
 import PolyHealthCenter.model.Prenotazione;
 import PolyHealthCenter.model.Sede;
 import PolyHealthCenter.repository.SedeDAORepository;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SedeService {
 	
 	@Autowired SedeDAORepository repo;
 	
+	//getBYID
 	public Sede getById(Long id) {
 		if (!repo.existsById(id)) {
 			throw new EntityExistsException("Comune non esiste!!");
@@ -26,6 +29,37 @@ public class SedeService {
 	public List<Sede> getAll() {
 		return repo.findAll();
 	}
+	
+	//create
+		public Sede createSede(Sede sede) {
+	    // Gestione di un errore | email già presente
+		  if(repo.existsByComune(sede.getComune()) ) {
+			throw new EntityNotFoundException(" la sede nella città di " + sede.getComune() +  " è già presente");
+			  }
+		  System.out.println("Sede inserita nel DB");
+		  return repo.save(sede);
+		}
+
+		
+		
+		//update
+		public Sede updateSede(Sede sede, Long id) {
+		  if (repo.existsById(id)) {
+			throw new EntityExistsException(" Sede [id:" + sede.getId() + "] non esiste");
+				}
+			repo.save(sede);
+			return sede;
+			}
+			
+			
+		//remove
+		public String removeSede(Long id) {
+		   if (!repo.existsById(id)) {
+			 throw new EntityExistsException("Sede con id " + id + " non è presente nel DB");
+		   }
+			repo.deleteById(id);
+			return "Sede cancellata!";
+			}
 	
 
 }
