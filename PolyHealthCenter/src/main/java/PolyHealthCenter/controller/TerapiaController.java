2799e2e6f2aaf6e3706758549e9b4beecca276a9
Eleonora Mattera <m.eleonora98@live.it>
2023-07-07 19:01:08 +0200

    add core to TerapiaContoller

--- PolyHealthCenter/src/main/java/PolyHealthCenter/controller/TerapiaController.java
+++ PolyHealthCenter/src/main/java/PolyHealthCenter/controller/TerapiaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import PolyHealthCenter.model.Terapia;
 import PolyHealthCenter.service.TerapiaService;
 
 
+@CrossOrigin(origins =  "*", maxAge = 360000)
 @RestController
 @RequestMapping("/api/terapie")
 public class TerapiaController {
